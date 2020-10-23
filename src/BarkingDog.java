17d894261d808497c2f7dfa30c7759554a03c6c7
Nikhil S N <73348331+nikhil02nayak@users.noreply.github.com>
2020-10-23 19:42:24 +0530

    Add files via upload
    Problem statement:
    We have a dog that likes to bark.  We need to wake up if the dog is barking at night!
    
    Write a method shouldWakeUp that has 2 parameters.
    
    1st parameter should be of type boolean and be named barking it represents if our dog is currently barking.
    2nd parameter represents the hour of the day and is of type int with the name hourOfDay and has a valid range of 0-23.
    
    We have to wake up if the dog is barking before 8 or after 22 hours so in that case return true.
    
    In all other cases return false.
    
    If the hourOfDay parameter is less than 0 or greater than 23 return false.

---
+++ src/BarkingDog.java
@@ -0,0 +1,29 @@
+public class BarkingDog
+{
+
+    public static boolean shouldWakeUp (boolean barking, int hourOfDay)
+    {
+        if (hourOfDay >= 0 && hourOfDay <= 23)
+        {
+          if(barking && (hourOfDay < 8 || hourOfDay > 22) )
+           {
+
+               return true ;
+           }
+          else if (barking && (hourOfDay < 0 && hourOfDay > 23))
+          {
+
+              return false ;
+          }
+          else
+          {
+
+              return false ;
+          }
+        }
+        else
+        {
+            return false ;
+        }
+    }
+}
